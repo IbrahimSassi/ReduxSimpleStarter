0151971f471c71da27f8666c866d82b96739e0ca
IbrahimSassi <sassiibrahim12@gmail.com>
2016-09-07 14:39:03 +0100

    AddingComponents + Youtube Search API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+
+const API_KEY = 'AIzaSyAzTmkufZ_2BKjdmc0gMD0yXlCbZJORIVY';
+
 // Create a new component , This Component should produce some Html
 const App = () => {
     return <div> Hi </div> ;
